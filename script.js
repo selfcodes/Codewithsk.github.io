f2f2d915c9196610be8443384e5dd32b36bab697
Coding4-Everyone <96863031+Coding4-Everyone@users.noreply.github.com>
2022-03-14 20:08:34 +0530

    Codewithsk

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init({
+  offset: 300
+});
+// var typed = new Typed(".auto-input", {
+//     strings: ["Developer", "Programmer", "CodeWithSk"],
+//     typeSpeed: 100,
+//     bcakSpeed: 100,
+//     loop: true
+// });
